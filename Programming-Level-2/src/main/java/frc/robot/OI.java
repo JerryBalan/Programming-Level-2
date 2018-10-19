53c2b09f3b649fd213ccf49fb2e014287498be3c
JerryBalan <jerrybalan@live.com>
2018-10-18 17:12:14 -0700

    started writing drivetrain tutorial again with aaron

--- Programming-Level-2/src/main/java/frc/robot/OI.java
+++ Programming-Level-2/src/main/java/frc/robot/OI.java
@@ -7,11 +7,18 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+  public OI() {
+    driveStick = new Joystick(0);
+    operatorStick = new Joystick(1);
+  }
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
@@ -19,7 +26,20 @@ public class OI {
   // number it is.
   // Joystick stick = new Joystick(port);
   // Button button = new JoystickButton(stick, buttonNumber);
+  public final Joystick driveStick;
+  public final Joystick operatorStick;
 
+  public double getDriveX() {
+    return driveStick.getX();
+  }
+
+  public double getDriveY() {
+    return driveStick.getY();
+  }
+
+  public double getDriveZ() {
+    return driveStick.getZ();
+  }
   // There are a few additional built in buttons you can use. Additionally,
   // by subclassing Button you can create custom triggers and bind those to
   // commands the same as any other Button.
